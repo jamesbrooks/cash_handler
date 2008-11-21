5c65f27e79b4d18130f65556e3c4d78936ca9448
James Brooks <james@gooddogdesign.com>
2008-11-21 10:59:55 +1030

    Added fetching exchange rates as values against rates other than the USD. Made changing the cache TTL easier (during creation)

--- lib/cash_handler/base.rb
+++ lib/cash_handler/base.rb
@@ -2,18 +2,19 @@ module CashHandler
   class Base
     attr_accessor :cache
     
-    def initialize
-      @cache = CashHandler::Cache.new
+    # Init the CashHandler (and cache), with a default TTL of 1 day
+    def initialize(ttl=1.day)
+      @cache = CashHandler::Cache.new(ttl)
     end
     
     # Fetches the exchange rate of a currency against the USD
-    def get(code)
-      @cache.get(code.to_s.upcase)
+    def get(code, options={:against => :usd})
+      @cache.get(code.to_s.upcase) / @cache.get(options[:against].to_s.upcase)
     end
     
     # Converts a value from one currency to another
     def convert(value, from, to)
-      value * get(from) / get(to)
+      value * get(from, :against => to)
     end
   end
 end
--- lib/cash_handler/cache.rb
+++ lib/cash_handler/cache.rb
@@ -5,15 +5,15 @@ module CashHandler
     attr_accessor :ttl
     
     # Creates the cache with a default TTL (time to live) of 1 day. After this TTL has elapsed, cached values are refreshed
-    def initialize(ttl=1.day)
+    def initialize(ttl)
       @ttl = ttl
       @expires_at = ttl.from_now
       
-      expire_cache
+      expire
     end
     
     # Forces an expiry of the cache
-    def expire_cache
+    def expire
       @expires_at = ttl.from_now
       @rates = CashHandler::Parser.fetch_rates
     end
@@ -21,7 +21,7 @@ module CashHandler
     # Fetches a cached values (and refreshes the cache if it has expired)
     def get(code)
       # Ensure we have a collection of rates
-      expire_cache unless @rates && @rates.any? && @expires_at > Time.now
+      expire unless @rates && @rates.any? && @expires_at > Time.now
       
       if @rates[code]
         @rates[code]
